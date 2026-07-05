94faf4e4c7efea8cda024b30d59ab2f56bca629a
pushadapuvarshitha-blip <pushadapuvarshitha@gmail.com>
2026-07-05 13:42:51 +0530

    Zepto clone project added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to the E-commerce App</h1>
+      <p>Your app is now rendering correctly.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,38 @@
+// --- PRODUCT DATABASE ---
+const productsList = [
+  {
+    id: 1,
+    name: "Fresh Avocados (2 pcs)",
+    price: 120,
+    image: "https://www.bbassets.com/media/uploads/p/xxl/10000312_21-fresho-avocado.jpg",
+    desc: "Butter fruity, rich in healthy fats.",
+  },
+  {
+    id: 2,
+    name: "Whole Wheat Bread",
+    price: 45,
+    image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTeXGfuNQJNODQsD4U1xj0qB_rYFvrB10Gi5hcJtF1XzgiNnhfFCvCmD7cFdOnUmpxzAl9VlC7M1XroKmo_jdEcSv-w5fcd",
+    desc: "Freshly baked whole wheat bread.",
+  },
+  {
+    id: 3,
+    name: "Cow Milk (1L)",
+    price: 70,
+    image: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSWU1qSMgyhOnYL3QTOZCuMWo0frUk770Er2dkJ2L5OoqakuSgZg1WrYq0SI8AJenz9GCjLwfBJL2O1q7p3eqI0YVtjJH8fmw0mxoWDsuoV3KwpGtOcJasz",
+    desc: "Pasteurized fresh cow milk.",
+  },
+  {
+    id: 4,
+    name: "Dark Chocolate",
+    price: 150,
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTnuqqmiukJx_lim-xT7b4re01d7WEABCkSpqB2uGQoOiHOrD9shh3eO1NV1ITM9cBS_gheOiTIkVFgxy3bhH6IV6AIAXMdeqwr7KRRs1uCxMiuguTqdcRF",
+    desc: "70% Cocoa Dark Chocolate.",
+  },
+];
+
+// --- USER DATABASE (For Login) ---
+const usersList = [
+  { email: "user@test.com", password: "1235" },
+  { email: "admin@zepto.com", password: "admin" },
+  { email: "rahul@gmail.com", password: "rahul" },
+];
